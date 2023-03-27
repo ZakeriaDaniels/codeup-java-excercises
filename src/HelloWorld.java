a610ef06454c486fd4bbea531266277d600449ff
[Czakeria daniels <zakeria.daniels01@gmail.com>
2023-03-27 10:27:53 -0500

    finished

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,29 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        System.out.println("Hello, World!");
+//    }
+        int myFavoriteNumber = 17;
+
+        System.out.println(myFavoriteNumber);
+
+        String myString = "I like playing with strings.";
+
+        System.out.println(myString);
+
+//        myString = 'd';
+//       myString = 3.14159;
+//        long myNumber = 0L;
+        float myNumber;
+        myNumber = (float)3.14;
+//        myNumber = 123L;
+//        myNumber = 123;
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+
+
     }
 }
