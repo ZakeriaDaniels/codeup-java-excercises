8ef36a9d03ae14b72940997db349eff5202f3dec
[Czakeria daniels <zakeria.daniels01@gmail.com>
2023-03-27 12:10:21 -0500

    doing 1 b

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -10,11 +10,16 @@ public class ControlFlowExercises {
 //            System.out.println(i);
 //            i += 2;
 //        } while (i <= 100);
-        int i = 100;
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+        int i = 2;
         do {
-            System.out.println(i);
-            i -= 5;
-        } while (i >= -10);
+            System.out.println(i * i);
+            i++;
+        } while (i * i < 1000000);
 
     }
 }
