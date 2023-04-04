48e3e72aab34b3bad2123138432689ee73c8023f
[Czakeria daniels <zakeria.daniels01@gmail.com>
2023-04-03 22:54:45 -0500

    finished

---
+++ src/Person.java
@@ -0,0 +1,33 @@
+public class Person {
+    private String name;
+    public Person(String name) {
+        this.name = name;
+    }
+
+
+    public String getName() {
+//TODO: return the person's name
+        return name;
+
+    }
+
+    public void setName(String name) {
+//TODO: change the name field to the passed value
+        this.name = name;
+    }
+
+    public void sayHello() {
+//TODO: print a message to the console using the person's name
+        System.out.println("Hello, my name is " + name);
+    }
+    public static void main(String[] args) {
+        Person person = new Person("Alice");
+        System.out.println("Person's name is " + person.getName());
+        person.setName("bob");
+        System.out.println("Person's name is " + person.getName());
+        person.sayHello();
+    }
+
+
+
+}
